46eaa051eebfba607f01928f61e0c5e93f1a8b59
dkpatt <dkpatt@gmail.com>
2017-05-04 11:58:07 -0700

    Added Given_SingleElementArrays__1__2

---
+++ Kata/KataTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void Given_SingleElementArrays__1__2()
+        {
+            var listFirst = new List<int>() { 1 };
+            var listSecond = new List<int>() { 2 };
+
+            var sandbox = new Sandbox();
+            var unionedList = sandbox.UnionSortedLists(listFirst, listSecond);
+
+            Assert.AreEqual(unionedList.Count, 2);
+            Assert.AreEqual(1, unionedList[0]);
+            Assert.AreEqual(2, unionedList[1]);
+        }
+    }
+}
---
+++ Kata/Sandbox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    class Sandbox
+    {
+        public Sandbox()
+        { }
+
+        public List<int> UnionSortedLists(List<int> firstSortedList, List<int> secondSortedList)
+        {
+            var unionedList = new List<int>();
+
+            unionedList.Add(firstSortedList[0]);
+            unionedList.Add(secondSortedList[0]);
+
+            return unionedList;
+        }
+    }
+}
